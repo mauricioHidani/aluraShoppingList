b89f2599fa88fbd0ea9c74ebf8106844c27752de
MauricioHidani <mauricio.hidani.work@gmail.com>
2024-10-09 16:55:50 -0300

    feat: start shopping cart

--- src/main/java/br/com/alura/challenges/shopping/list/Main.java
+++ src/main/java/br/com/alura/challenges/shopping/list/Main.java
@@ -1,7 +1,18 @@
 package br.com.alura.challenges.shopping.list;
 
+import br.com.alura.challenges.shopping.list.controllers.ShoppingListController;
+import br.com.alura.challenges.shopping.list.services.impls.BuyerService;
+import br.com.alura.challenges.shopping.list.services.impls.ShoppingService;
+
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		final var scanner = new Scanner(System.in);
+		final var buyerService = new BuyerService(scanner);
+		final var productService = new ShoppingService(scanner);
+		final var controller = new ShoppingListController(buyerService, productService);
+
+		controller.start();
 	}
 }
